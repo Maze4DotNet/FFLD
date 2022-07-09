7e729ec5dcaa9871707c7977eb1170cef6e15932
Maze4DotNet <m.verheije@4dotnet.nl>
2022-07-09 15:53:43 +0200

    Added sword that turns around when the character does, moves with the character, and changes size when the Attack level changes.

---
+++ Assets/Scripts/Sword/SwordSize.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FFLD
+{
+    public class SwordSize : MonoBehaviour
+    {
+        private int _lastKnownAttackLevel;
+        private Transform _transform;
+        private Vector2 _originalScale;
+        public CharacterSheet _characterSheet;
+
+        private void Awake()
+        {
+            _transform = GetComponent<Transform>();
+            _originalScale = transform.localScale;
+            _lastKnownAttackLevel = _characterSheet.AttackLevel;
+
+        }
+
+        private void Update()
+        {
+            if (_lastKnownAttackLevel != _characterSheet.AttackLevel)
+            {
+                _lastKnownAttackLevel = _characterSheet.AttackLevel;
+                float factor = (1f + _lastKnownAttackLevel) / 6f;
+                _transform.localScale = new Vector3(_originalScale.x * factor, _originalScale.y * factor);
+            }
+        }
+    }
+}
