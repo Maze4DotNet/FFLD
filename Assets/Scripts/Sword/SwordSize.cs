0b00641e7cf0b2ac39b8f1974e29e08fc44e24d6
Maze4DotNet <m.verheije@4dotnet.nl>
2022-07-09 16:02:27 +0200

    test commit dit zou niet moeten kunnen

--- Assets/Scripts/Sword/SwordSize.cs
+++ Assets/Scripts/Sword/SwordSize.cs
@@ -19,7 +19,7 @@ namespace FFLD
             _transform = GetComponent<Transform>();
             _originalScale = transform.localScale;
             _lastKnownAttackLevel = _characterSheet.AttackLevel;
-
+            1 = 1;
         }
 
         private void Update()
