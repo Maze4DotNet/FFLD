0c1c93d61703b58026bdc2cfa7697642742f8147
Kirbman10 <38724165+Kirbman10@users.noreply.github.com>
2022-07-10 12:51:56 +0200

    first behaviououri

---
+++ Assets/Scripts/Enemies/EnemyJump.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJump : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)] private float _jumpHeight = 3f;
+    [SerializeField, Range(0f, 5f)] private float _downwardMovementMultiplier = 3f;
+    [SerializeField, Range(0f, 5f)] private float _upwardMovementMultiplier = 1.7f;
+
+    public bool IsAirborne { get { return !_ground.OnGround; } }
+
+    private Rigidbody2D _body;
+    private Ground _ground;
+    private Vector2 _velocity;
+
+    private float _defaultGravityScale, _jumpSpeed;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _ground = GetComponent<Ground>();
+
+        _defaultGravityScale = 1f;
+    }
+
+    private void FixedUpdate()
+    {
+        _velocity = _body.velocity;
+
+        if (_body.velocity.y > 0)
+        {
+            _body.gravityScale = _upwardMovementMultiplier;
+        }
+        else if (_body.velocity.y < 0)
+        {
+            _body.gravityScale = _downwardMovementMultiplier;
+        }
+        else if (_body.velocity.y == 0)
+        {
+            _body.gravityScale = _defaultGravityScale;
+        }
+
+        _body.velocity = _velocity;
+    }
+
+    private void JumpAction()
+    {
+        _jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * _jumpHeight);
+
+        if (_velocity.y > 0f)
+        {
+            _jumpSpeed = Mathf.Max(_jumpSpeed - _velocity.y, 0f);
+        }
+        else if (_velocity.y < 0f)
+        {
+            _jumpSpeed += Mathf.Abs(_body.velocity.y);
+        }
+        _velocity.y += _jumpSpeed;
+    }
+}
