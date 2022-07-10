aa6d5daa0c881197ddfc57f0e5daa510e62d4bd1
Kirbman10 <38724165+Kirbman10@users.noreply.github.com>
2022-07-10 19:16:23 +0200

    menu pause

--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
-    public GameObject _menuUI;
     // Start is called before the first frame update
     void Awake()
     {
-        _menuUI.SetActive(true);
         Time.timeScale = 0f;
     }
 
     // Update is called once per frame
     void OnDestroy()
     {
-        _menuUI.SetActive(false);
         Time.timeScale = 1f;
     }
 }
