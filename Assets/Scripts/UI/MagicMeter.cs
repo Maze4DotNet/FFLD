e022cde8a2bb3bd59de5d79e13fcd80ef4482cee
Maze4DotNet <m.verheije@4dotnet.nl>
2022-07-10 14:54:26 +0200

    Added hearts, energies and mana meter.

---
+++ Assets/Scripts/UI/MagicMeter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+internal class MagicMeter : MonoBehaviour
+{
+    public CharacterSheet _characterSheet;
+    public Image _magicMeter;
+    public Image _manaValue;
+
+    public Sprite _magicMeterSprite;
+    public Sprite _manaValueSprite;
+    public Sprite _nothing;
+
+
+    private void Update()
+    {
+        if (_characterSheet.MagicLevel == 0)
+        {
+            _magicMeter.sprite = _nothing;
+            _manaValue.sprite = _nothing;
+            return;
+        }
+        _manaValue.transform.localScale = new Vector3(_characterSheet.Mana *3.17f/ 100, 3.17f, 3.17f);
+        _magicMeter.sprite = _magicMeterSprite;
+        _manaValue.sprite = _manaValueSprite;
+    }
+}
